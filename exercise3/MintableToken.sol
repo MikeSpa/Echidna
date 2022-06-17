89aa2cadc21b33efe7db4c9f0680bf346dfadda7
Spahr Michael <spahr.michael@protonmail.com>
2022-06-17 16:50:32 +0200

    fix vulnerability in MintableToken.sol

--- exercise3/MintableToken.sol
+++ exercise3/MintableToken.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.5.11;
 import "./Token.sol";
 
 contract MintableToken is Token {
-    int256 totalMinted;
-    int256 totalMintable;
+    uint256 totalMinted;
+    uint256 totalMintable;
 
-    constructor(int256 _totalMintable) public {
+    constructor(uint256 _totalMintable) public {
         totalMintable = _totalMintable;
     }
 
     function mint(uint256 value) public isOwner {
-        require(int256(value) + totalMinted < totalMintable);
-        totalMinted += int256(value);
+        require(uint256(value) + totalMinted < totalMintable);
+        totalMinted += uint256(value);
         balances[msg.sender] += value;
     }
 }
