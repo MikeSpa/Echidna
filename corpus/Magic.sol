4909720ed6faeb27ce8981c06f813dcdda9404d8
Spahr Michael <spahr.michael@protonmail.com>
2022-06-18 15:59:01 +0200

    collect and modify corpus

---
+++ corpus/Magic.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.5.11;
+
+contract Magic {
+    bool value_found = false;
+
+    function magic(
+        uint256 magic_1,
+        uint256 magic_2,
+        uint256 magic_3,
+        uint256 magic_4
+    ) public {
+        require(magic_1 == 42);
+        require(magic_2 == 129);
+        require(magic_3 == magic_4 + 333);
+        value_found = true;
+        return;
+    }
+
+    function echidna_magic_values() public returns (bool) {
+        return !value_found;
+    }
+}
